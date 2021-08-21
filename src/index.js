132c36ec4a09027271ac81099e0066ca61c86d6f
Ilya Gutorov <gutorov.iliya@gmail.com>
2021-08-21 19:00:34 +0300

    feat: add code task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let result = Math.abs(n).toFixed(0).split('').reverse().join('');
+    return Number(result);
 }
